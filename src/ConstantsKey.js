24bbd8e34ddd83cab873f43a854f827a5459ad2f
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-05-06 19:04:09 +0200

    clean up

--- src/ConstantsKey.js
+++ src/ConstantsKey.js
@@ -3,6 +3,13 @@ export const Tileset = {
   key: "tileset"
   };
 
+/*
+path : link to the file
+key: Always put "TEST",
+lastLayer: number of layers available in the level. 3 for only one layer, 1, for three layers,
+text: Text to explain the level
+playerPostion: original position of the player with the format {x: valueX, y: valueY}
+*/
 export const Level1 = {
   path: "LVL_1.json",
   key: "Test",
@@ -11,55 +18,8 @@ export const Level1 = {
   playerPosition: {x: 64, y: 352}
 };
 
-export const Level2 = {
-  path: "LVL_6.json",
-  key: "Test",
-  lastLayer: 3,
-  text: "",
-  playerPosition: {x: 64, y: 20}
-};
-
-export const Level3 = {
-  path: "LVL_2.json",
-  key: "Test",
-  lastLayer: 2,
-  text: "The gap seems too large. You might want to use your power",
-  playerPosition: {x: 64, y: 352}
-};
-
-export const Level4 = {
-  path: "LVL_7.json",
-  key: "Test",
-  lastLayer: 2,
-  text: "",
-  playerPosition: {x: 64, y: 20}
-};
-
-
-export const Level5 = {
-  path: "LVL_3.json",
-  key: "Test",
-  lastLayer: 2,
-  text: "A layer can help you, but it could stop you too. Press Shift to rollback.",
-  playerPosition: {x: 64, y: 352}
-};
-
-export const Level6 = {
-  path: "LVL_4.json",
-  key: "Test",
-  lastLayer: 1,
-  text: "You may need to remove more than one layer",
-  playerPosition: {x: 64, y: 352}
-};
-
-
 export const Levels = {
-  Level1,
-  Level2,
-  Level3,
-  Level4,
-  Level5,
-  Level6
+  Level1
 };
 
 export const HeroSprite = {
